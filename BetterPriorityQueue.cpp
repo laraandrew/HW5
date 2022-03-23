ecf402d3ea23af35c3caffc21b7447c98d101f4c
laraandrew <aandrewlaraa@gmail.com>
2022-03-23 13:40:24 -0400

    finally no errors

--- BetterPriorityQueue.cpp
+++ BetterPriorityQueue.cpp
@@ -2,8 +2,29 @@
 #include <queue>
 #include <vector>
 #include <iostream>
+#include <climits>
+#include "TemplateGraph.h"
 
-class BetterPriorityQueue: public priority_queue<DNode, vector<DNode>, 
-greater<DNode>>::priority_queue { 
-// ... your code goes here ...
+using namespace std;
+
+struct DNode {
+    int pri = INT_MAX;
+    bool visited = false;
+
+    const GraphNode *node = nullptr;
+
+    bool operator<(const DNode &other) const {
+        return pri < other.pri;
+    }
+    bool operator>(const DNode &other) const {
+        return pri > other.pri;
+    }
+    bool operator==(const DNode &other) const {
+        return node == other.node;
+    }
+};
+
+class BetterPriorityQueue: public priority_queue<DNode, vector<DNode>, greater<DNode>>::priority_queue { 
+
+
 };
