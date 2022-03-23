9400d6a7e747c13666d46a862d9ee2a68245360f
laraandrew <aandrewlaraa@gmail.com>
2022-03-23 13:40:51 -0400

    update with included structs and Graph template

--- Graph.cpp
+++ Graph.cpp
@@ -3,3 +3,34 @@
 #include <vector>
 #include <iostream>
 
+#include "TemplateGraph.h"
+
+class Graph {
+
+    GraphNode *AddNode(char key, int data = 0){
+
+    }
+	GraphEdge *AddEdge(GraphNode *gn1, GraphNode *gn2, unsigned int weight = 0){
+
+    }
+		
+	string NodesToString() const{
+
+    }
+	string ToString() const{
+        
+    }
+
+	static string GraphNodeToString(const GraphNode *gn);
+	static string GraphEdgeToString(const GraphEdge *ge);
+		
+	// https://stackoverflow.com/questions/15106690/how-does-ampersand-in-the-return-type-of-a-function-declaration-work
+    const vector<GraphEdge*>& GetEdges(const GraphNode *gn) const;
+	const vector<GraphNode*>& GetNodes() const;
+
+	const GraphNode* NodeAt(unsigned int idx) const;
+		
+	size_t Size() const; // the number of edges
+	size_t Order() const; // the number of nodes
+
+};
--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 all:
-	g++ -Wall Graph.cpp GraphTests.cpp -o graph-tests
-	g++ -Wall BetterPriorityQueue.cpp BPQTests.cpp -o bpq-tests
+	g++ -Wall Graph.cpp -o graph-tests
+	g++ -Wall BetterPriorityQueue.cpp -o bpq-tests
 run: 
 	./graph-tests
 	./bpq-tests
